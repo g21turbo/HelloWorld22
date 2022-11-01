ed1e95b01d9a4fbcb8a4125c4881a1c6c4072afc
User <dg21stew@gmail.com>
2022-11-01 11:15:52 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+//Class
+public class Main {
+
+    //Main method
+    public static void main(String[] args) {
+
+        //sout - hello world
+        System.out.println("Hello World");
+
+
+    }
+
+
+}
